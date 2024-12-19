e236b6431fcf228faf48bbc0f870a84373200b71
vasilistheiou <vtheiou@outlook.com.gr>
2024-12-19 15:57:28 +0200

    Integrated AST

--- models/AST/AST_model.py
+++ models/AST/AST_model.py
@@ -4,25 +4,26 @@ import json
 from typing import Optional, Dict
 
 import torch
+import torchaudio
 import librosa
 import numpy as np
 
 DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
-from beats_modules.BEATs import BEATsConfig, BEATs
+from ast_modules.src.models import ASTModel
 
 
-class BEATs_Model:
+class AST_Model:
 
-    # Initialize BEATs Model
+    # Initialize AST Model
     def __init__(self,
                  path_to_checkpoint: str,
                  path_to_ontology: str = os.path.join(DIR_PATH, "ontology.json"), 
                  device: Optional[str] = None,
                  hypercategory_mapping: Optional[os.PathLike] = None):
         """
-        Initalize the BEATs model
+        Initalize the AST model
 
         Args:
             path_to_checkpoint (string): The path to the checkpont file that you downloaded.
@@ -33,16 +34,8 @@ class BEATs_Model:
         """
         self.path_to_checkpoint = path_to_checkpoint
 
-        # Load checkpoint.
-        checkpoint = torch.load(self.path_to_checkpoint)
-        
-        # Dictionary containing index and class name.
+        # Dictionary containing 
         self.ontology = parse_ontology(path_to_ontology)
-
-        cfg = BEATsConfig(checkpoint['cfg'])
-        self.model = BEATs(cfg)
-        self.model.load_state_dict(checkpoint['model'])
-        self.model.eval()
 
         if device == "cuda" and torch.cuda.is_available():
             self.device = device
@@ -50,26 +43,79 @@ class BEATs_Model:
         else:
             self.device = "cpu"
 
+        self.model = self._load_ast_model(path_to_checkpoint)
+
+
         if hypercategory_mapping is not None:
             self.map_to_hypercategories(hypercategory_mapping)
         else:
             self.hypercategory_mapping = np.array([])
+
+    def _load_ast_model(self, path_to_checkpoint):
+        model = ASTModel(
+            label_dim=527,  # AudioSet has 527 classes
+            fstride=10,
+            tstride=10,
+            input_fdim=128,
+            input_tdim=1024,
+            imagenet_pretrain=True,
+            audioset_pretrain=False,
+            model_size='base384'
+        )
+
+        checkpoint = torch.load(path_to_checkpoint, map_location='cuda')
+        audio_model = torch.nn.DataParallel(model, device_ids=[0])
+        audio_model.load_state_dict(checkpoint)
+        audio_model = audio_model.to(self.device)
+      
+
+        return model
+    
+    def _make_features(self, waveform, mel_bins = 128, target_length=1024, sr = 16000):
+        # waveform, sr = torchaudio.load(wav_name)
+
+        fbank = torchaudio.compliance.kaldi.fbank(
+            waveform, htk_compat=True, sample_frequency=sr, use_energy=False,
+            window_type='hanning', num_mel_bins=mel_bins, dither=0.0,
+            frame_shift=10)
+
+        n_frames = fbank.shape[0]
+
+        p = target_length - n_frames
+        if p > 0:
+            m = torch.nn.ZeroPad2d((0, 0, 0, p))
+            fbank = m(fbank)
+        elif p < 0:
+            fbank = fbank[0:target_length, :]
+
+        fbank = (fbank - (-4.2677393)) / (4.5689974 * 2)
+        return fbank
+
 
     def make_inference_with_path(self, path_to_audio):
         """Method to make a prediction using a file path
 
         path_to_audio -- Path to the target audio file
         """
+        
+        # Load wavefrom
 
-        # Load waveform
-        audio, _ = librosa.load(path_to_audio, sr=16000)
-        audio = torch.Tensor(audio).unsqueeze(0).to(self.device)
+        audio, _ = librosa.load(path_to_audio, sr = 16000, mono = True)
+        audio = torch.from_numpy(audio).unsqueeze(0)
 
+        
+        feats = self._make_features(audio)
+
+        input_tdim = feats.shape[0]
+        
+        feats_data = feats.expand(1, input_tdim, 128)  
+        
         # Make prediction
         with torch.no_grad():
-            probs = self.model.extract_features(audio)[0][0]
-            probs = probs.cpu().numpy()
+            output = self.model.forward(feats_data)
+            output = torch.sigmoid(output)
 
+        probs = output.data.cpu().numpy()[0]
         # Get Index and Class name of prediction
         max_idx = np.argmax(probs)
 
@@ -85,13 +131,20 @@ class BEATs_Model:
         waveform -- The audio waveform
         """
 
-        # Load waveform
-        waveform = torch.Tensor(waveform).unsqueeze(0).to(self.device)
+        waveform = torch.from_numpy(waveform).unsqueeze(0)
+
+        feats = self._make_features(waveform, mel_bins=128)
+
+        input_tdim = feats.shape[0]
+        
+        feats_data = feats.expand(1, input_tdim, 128)  
 
         # Make prediction
         with torch.no_grad():
-            probs = self.model.extract_features(waveform)[0][0]
-            probs = probs.cpu().numpy()
+            output = self.model.forward(feats_data)
+            output = torch.sigmoid(output)
+
+        probs = output.data.cpu().numpy()[0]
 
         # Get Index and Class name of prediction
         max_idx = np.argmax(probs)
@@ -114,3 +167,4 @@ def parse_ontology(path_to_ontology):
     ontology_list = {int(k): v for k, v in ontology_list.items()}
 
     return ontology_list
+
--- utils/init_utils.py
+++ utils/init_utils.py
@@ -7,16 +7,28 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import torch
 
 from models.beats.beats_model import BEATs_Model
+from models.PaSST.passt_model import Passt_Model
+from models.AST.AST_model import AST_Model
 from algorithms.differential_evolution.Differential_Evolution import DifferentialEvolutionAttacker
 from algorithms.pso.pso_attacker import PSO_Attacker
 
 
-def get_model(model_str: str, model_pt_file: Optional[str] = None, hypercategory_mapping: Optional[Dict] = None):
+def get_model(model_str: str,
+              model_pt_file: Optional[str] = None,
+              hypercategory_mapping: Optional[Dict] = None):
     if model_str == 'beats':
         device = "cuda" if torch.cuda.is_available() else "cpu"
         model = BEATs_Model(path_to_checkpoint=model_pt_file,
                             device=device,
                             hypercategory_mapping=hypercategory_mapping)
+    elif model_str == "passt":
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        model = Passt_Model(device=device,
+                            hypercategory_mapping=hypercategory_mapping)
+
+    else:
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        model = AST_Model(path_to_checkpoint=model_pt_file, device=device, hypercategory_mapping=hypercategory_mapping)
     return model
 
 
@@ -27,7 +39,7 @@ def init_algorithm(algorithm: str,
                    objective_function=None,
                    target_class=None,
                    hypercategory_target=None):
-    
+
     if algorithm == 'de':
         ATTACK_ALGORITHM = DifferentialEvolutionAttacker(model=model,
                                                          verbosity=verbosity,
