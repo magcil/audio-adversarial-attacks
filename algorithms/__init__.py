e8972d7ca4d441bbb4915103c59acbeb1d20a8c2
ChrisNick92 <chrisnick92@gmail.com>
2025-01-17 10:48:35 +0200

    Change Default algorithm hyperparameters

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,17 +1,17 @@
 # Default Hyperparameters for PSO & DE these will be used if not specified
 
 default_pso_hyperparams = {
-    "initial_particles": 35,
+    "initial_particles": 25,
     "max_iters": 20,
     "max_inertia_w": 0.9,
     "min_inertia_w": 0.1,
-    "memory_w": 0.3,
+    "memory_w": 1.2,
     "information_w": 1.2,
     "perturbation_ratio": 0.5
 }
 
 default_de_hyperparams = {
-    "pop_size": 15,
+    "pop_size": 20,
     "iter": 10,
     "F": 1.2,
     "cr": 0.9,
