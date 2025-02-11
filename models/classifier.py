b933ab31cef2b07950b6c31d3e8b303cfc9ff36f
vasilistheiou <vtheiou@outlook.com.gr>
2025-02-11 12:57:37 +0200

    Added config file and fixed bugs

--- models/classifier.py
+++ models/classifier.py
@@ -1,6 +1,8 @@
 import os
 import sys
+import yaml
 from pathlib import Path
+import argparse
 
 PROJECT_PATH = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
@@ -15,67 +17,68 @@ from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from sklearn.metrics import accuracy_score, f1_score
 
-
-
-
 from utils.init_utils import get_model
 from models.callbacks import EarlyStopping
+from datasets.datasets import ESC50Dataset
+
 
 class FineTuneClassifier(nn.Module):
-    def __init__(self, model_name, num_classes = 5, weight_path = None, hypercategory_mapping = None, freeze_backbone = True, device = "cpu"):
+
+    def __init__(self,
+                 model_name,
+                 num_classes=5,
+                 weight_path=None,
+                 hypercategory_mapping=None,
+                 freeze_backbone=True,
+                 device="cpu"):
         super(FineTuneClassifier, self).__init__()
 
         self.model_name = model_name
         self.device = device
 
         if model_name == "beats":
-            self.backbone = get_model(model_str=model_name, model_pt_file=weight_path,hypercategory_mapping = hypercategory_mapping)
+            self.backbone = get_model(
+                model_str=model_name,
+                model_pt_file=weight_path,
+                hypercategory_mapping=hypercategory_mapping, device=device)
             in_features = 768
             pass
         elif model_name == "passt":
-            self.backbone = get_model(model_str=model_name, hypercategory_mapping = hypercategory_mapping)
+            self.backbone = get_model(
+                model_str=model_name,
+                hypercategory_mapping=hypercategory_mapping, device=device)
             self.backbone.model.net.head = nn.Identity()
             in_features = 768
             pass
         else:
             raise ValueError(f"Unsupported model: {model_name}")
-        
+
         if freeze_backbone:
             for param in self.backbone.model.parameters():
                 param.requires_grad = False
 
-        self.classifier = nn.Sequential(
-            nn.Linear(in_features, 512),
-            nn.BatchNorm1d(512),
-            nn.ReLU(),
-            nn.Linear(512, 256),
-            nn.BatchNorm1d(256),
-            nn.ReLU(),
-            nn.Linear(256, 128),
-            nn.BatchNorm1d(128),
-            nn.ReLU(),
-            nn.Linear(128, num_classes)
-        )
-
+        self.classifier = nn.Sequential(nn.Linear(in_features, 512),
+                                        nn.BatchNorm1d(512), nn.ReLU(),
+                                        nn.Linear(512,
+                                                  256), nn.BatchNorm1d(256),
+                                        nn.ReLU(), nn.Linear(256, 128),
+                                        nn.BatchNorm1d(128), nn.ReLU(),
+                                        nn.Linear(128, num_classes))
 
     def forward(self, x):
 
         if self.model_name == "beats":
-            x = torch.Tensor(waveform).unsqueeze(0).to(self.device)
             embeddings = self.get_embeddings_BEATs(x)
         else:
-            waveform = x.astype(np.float32)
-            waveform = torch.from_numpy(waveform).unsqueeze(0).to(self.device)
-
             with torch.no_grad():
-                embeddings = self.backbone.model(waveform)
+                embeddings = self.backbone.model(x)
 
         output = self.classifier(embeddings)
         return output
 
     def get_embeddings_BEATs(self, x):
         with torch.no_grad():
-            _, _ , embeddings = self.backbone.model.extract_features(x)
+            _, _, embeddings = self.backbone.model.extract_features(x)
         embeddings = embeddings.mean(dim=1)
         return embeddings
 
@@ -90,44 +93,57 @@ def training_loop(model,
                   pt_file,
                   num_workers,
                   weight_decay,
-                  gamma=0, device = "cpu"):
+                  device="cpu"):
     """Training a deep neural network on the training dataset of AudioSet"""
 
-    # Initialize loss function 
-    criterion = nn.CrossEntropyLoss()
+    # Initialize loss function
+    criterion = nn.CrossEntropyLoss().to(device)
 
-    criterion = criterion.to(device)
-    model = FineTuneClassifier(model_name="beats", num_classes=5, freeze_backbone=True).to(device)
+    model.to(device)
 
     # Initialize dataloaders
-    train_dloader = DataLoader(train_dset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-    val_dloader = DataLoader(val_dset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    train_dloader = DataLoader(train_dset,
+                               batch_size=batch_size,
+                               shuffle=True,
+                               num_workers=num_workers)
+    val_dloader = DataLoader(val_dset,
+                             batch_size=batch_size,
+                             shuffle=False,
+                             num_workers=num_workers)
 
     # Initialize optimizer, learning rate shedulers and early stopping.
-    optim = Adam(model.classifier.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    optim = Adam(model.classifier.parameters(),
+                 lr=learning_rate,
+                 weight_decay=weight_decay)
 
-    lr_scheduler = CosineAnnealingLR(optimizer=optim, T_max=epochs, eta_min=1e-6)
+    lr_scheduler = CosineAnnealingLR(optimizer=optim,
+                                     T_max=epochs,
+                                     eta_min=1e-6)
     early_stopping = EarlyStopping(patience=patience,
                                    verbose=True,
-                                   path=os.path.join(PROJECT_PATH, "pretrained_models", f"{pt_file}"))
+                                   path=os.path.join(PROJECT_PATH,
+                                                     "pretrained_models",
+                                                     f"{pt_file}"))
 
     train_loss, val_loss = 0.0, 0.0
     _padding = len(str(epochs + 1))
 
-
     for epoch in range(1, epochs + 1):
+        model.train()
 
-        model.backbone.model.train()
-
-        with tqdm(train_dloader, unit="batch", leave=False, desc="Training set") as tbatch:
+        with tqdm(train_dloader,
+                  unit="batch",
+                  leave=False,
+                  desc="Training set") as tbatch:
             for i, item in enumerate(tbatch, 1):
                 # Forward pass
-                X_spec = item['audio_features']
-                X_spec = X_spec.to(device)
-                label = item['label'].to(device)
+                X_wave = item['waveform'].to(device)
 
-                output = model.forward(X_spec)
-                loss = (output, label)
+                label = item['label'].to(device)
+                output = model.forward(X_wave)
+
+                loss = criterion(output, label)
+
                 train_loss += loss.item()
 
                 # Backward pass
@@ -140,23 +156,30 @@ def training_loop(model,
         model.eval()
         y_true, y_pred = [], []
         with torch.no_grad():
-            with tqdm(val_dloader, unit="batch", leave=False, desc="Validation set") as vbatch:
+            with tqdm(val_dloader,
+                      unit="batch",
+                      leave=False,
+                      desc="Validation set") as vbatch:
                 for i, item in enumerate(vbatch, 1):
-                    X_spec = item['audio_features']
-                    X_spec = X_spec.to(device)
+                    X_wave = item['waveform'].to(device)
                     label = item['label'].to(device)
 
-                    output = model.forward(X_spec)
+                    output = model.forward(X_wave)
 
-                    loss = (output, label)
+                    loss = criterion(output, label)
+
                     val_loss += loss.item()
                     y_true.append(label.cpu().numpy())
                     batch_preds = output.argmax(axis=1).detach().cpu().numpy()
                     y_pred.append(batch_preds)
         y_true, y_pred = np.concatenate(y_true), np.concatenate(y_pred)
         val_loss /= len(val_dloader)
-        print(f"Epoch {epoch:<{_padding}}/{epochs}. Train Loss: {train_loss:.3f}. Val Loss: {val_loss:.3f}. ", sep="")
-        print(f"Val acc: {accuracy_score(y_true, y_pred):.3f}. Val F1: {f1_score(y_true, y_pred, average='macro'):.3f}")
+        print(
+            f"Epoch {epoch:<{_padding}}/{epochs}. Train Loss: {train_loss:.3f}. Val Loss: {val_loss:.3f}. ",
+            sep="")
+        print(
+            f"Val acc: {accuracy_score(y_true, y_pred):.3f}. Val F1: {f1_score(y_true, y_pred, average='macro'):.3f}"
+        )
         early_stopping(val_loss, model, epoch)
         if early_stopping.early_stop:
             print("Early Stopping.")
@@ -164,34 +187,71 @@ def training_loop(model,
         train_loss, val_loss = 0.0, 0.0
 
 
-def validation_loop(model, path_to_pt_file, test_dset, batch_size, num_workers, device = "cpu"):
-    """Validating the model on the validation set of AudioSet"""
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog="run_attack_dev.py",
+        description=
+        "Perform an attack on a path of wav files on a pretrained model")
+    parser.add_argument("-c",
+                        "--config_file",
+                        required=True,
+                        help="Configuration file (.yaml) for the attack.")
 
-    model.load_state_dict(torch.load(path_to_pt_file))
-    model = model.to(device)
-    model.eval()
+    return parser.parse_args()
 
-    test_dloader = DataLoader(test_dset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-
-    y_true, y_pred, posteriors = [], [], []
-
-    with torch.no_grad():
-        with tqdm(test_dloader, unit="batch", leave=False, desc="Test set") as vbatch:
-            for i, item in enumerate(vbatch, 1):
-                X_spec = item['audio_features']
-                X_spec = X_spec.to(device)
-                label = item['label']
-
-                logits = model.forward(X_spec)
-                probs = torch.softmax(logits, dim = 1)
-
-                y_true.append(label.numpy())
-                y_pred.append(results['preds'].numpy())
-                posteriors.append(results['probs'].numpy())
-
-    y_true, y_pred, posteriors = np.concatenate(y_true), np.concatenate(y_pred), np.concatenate(posteriors)
-
-    return {"y_true": y_true, "y_pred": y_pred, "posteriors": posteriors}
 
 if __name__ == "__main__":
-    pass
+
+    args = parse_args()
+    # Get Config file
+    with open(args.config_file, "r") as f:
+        config = yaml.safe_load(f)
+
+    json_hypercategory = config[
+        "esc_hypercategories"]  # Hypercategory Mapping
+    data_path = config["data_path"]  # Full Path to the Wav files of ESC-50
+    metadata_csv = config["metadata_csv"]  # Full Path to Metadata csv
+
+    train_folds = config["train_folds"]
+    test_folds = config["test_folds"]
+
+    train_dset = ESC50Dataset(data_path=data_path,
+                              metadata_csv=metadata_csv,
+                              hypercategory_mapping=json_hypercategory,
+                              folds=train_folds)
+    test_dset = ESC50Dataset(data_path=data_path,
+                             metadata_csv=metadata_csv,
+                             hypercategory_mapping=json_hypercategory,
+                             folds=test_folds)
+
+    model_name = config["model_architecture"]
+    path_to_checkpoint = config["model_pretrained_weights"]
+    hypercategories = config["hypercategory_mapping"]
+    device = config.get('device', 'cpu')
+
+    model = FineTuneClassifier(model_name=model_name,
+                               num_classes=5,
+                               weight_path=path_to_checkpoint,
+                               hypercategory_mapping=hypercategories,
+                               freeze_backbone=True,
+                               device=device)
+
+    # Trainning Parameters
+    batch_size = config["batch_size"]
+    epochs = config["epochs"]
+    learning_rate = float(config["learning_rate"])
+    patience = config["patience"]
+    pt_file = config["pt_file"]
+    num_workers = config["num_workers"]
+    weight_decay = config["weight_decay"]
+    training_loop(model=model,
+                  train_dset=train_dset,
+                  val_dset=test_dset,
+                  batch_size=batch_size,
+                  epochs=epochs,
+                  learning_rate=learning_rate,
+                  patience=patience,
+                  pt_file=pt_file,
+                  num_workers=num_workers,
+                  weight_decay=weight_decay,
+                  device=device)
